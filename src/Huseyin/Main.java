3d86cdb139201036cd3d6ba853aa46d288a79821
Hüseyin <hince1992@gmail.com>
2023-08-18 16:41:08 +0200

    geliştirme yapıldı

--- src/Huseyin/Main.java
+++ src/Huseyin/Main.java
@@ -4,9 +4,11 @@ public class Main {
     public static void main(String[] args) {
 
 
-        RentApartments obje = new RentApartments("Hüseyin", 2, true);
+        RentApartments obje = new RentApartments("Hüseyin", 2, true, true);
 
-        System.out.println(obje.getName() + " kullanıcısının kirası " + (obje.kiraHesapla()+ obje.balkonVarmi()));
+
+        System.out.println(obje.getName() + " kullanıcısının kirası " +
+                (obje.kiraHesapla() + obje.balkonVarmi() + obje.esyaliMi()));
 
 
     }
