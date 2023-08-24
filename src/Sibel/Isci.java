089dcba47c0794d423d5e3c143984297265d8e30
Sibel Tokgöz <Sibel.M.Tokgoez@gmail.com>
2023-08-24 16:47:24 +0200

    Degisiklikler

---
+++ src/Sibel/Isci.java
@@ -0,0 +1,14 @@
+package Sibel;
+
+public class Isci extends Personel {
+    String statu = "isci";
+    String imtiyaz = "sendika üyeligi hakkina sahiptir";
+    int yemekKarti= 2;
+
+    @Override
+    public void mesai() {
+        System.out.println("iscinin maasi günlük 10 saattir");
+    }
+
+   }
+
---
+++ src/Sibel/RunnerClass.java
@@ -0,0 +1,29 @@
+package Sibel;
+
+public class RunnerClass {
+    public static void main(String[] args) {
+        Ustabasi Ahmet = new Ustabasi();
+        System.out.println("Ustabasi Ahmet " );
+        System.out.println(Ahmet.statu);
+        System.out.println(Ahmet.imtiyaz);
+        System.out.println(Ahmet.yemekKarti);
+
+
+        System.out.println("*********");
+        System.out.println("Isci Ali");
+        Isci Ali = new Ustabasi();
+
+
+        System.out.println(Ali.statu);
+        System.out.println(Ali.imtiyaz);
+        System.out.println(Ali.yemekKarti);
+
+        Personel Ayse = new Personel();
+
+
+
+    }
+
+
+
+}
